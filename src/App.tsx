a19884075694719330ee5f3b74275479b7081ac9
Pedro Caetano <pedro.kaetano2003@gmail.com>
2025-01-12 22:19:17 -0300

    Desfazendo a ultima mudança

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { ToastProvider } from './components/ToastProvider';
 import ListaPaginada from './components/listaPaginada';
 import './index.css'
 import { Footer } from './components/footer';
-import AppIcon from './components/appIcon';
 
 export function App() {
   return (
@@ -12,7 +11,6 @@ export function App() {
       <ToastProvider>
         <main className="px-4 celular:px-8 tablet:px-16 tabletx:px-24 notebook:px-48 pt-8 tablet:pt-12 
             tabletx:pt-16 max-h-full">
-          <AppIcon defaultIcon="/icon-192x192.png" activeIcon="/icon-192x192-active.png" />
           <Hero />
           <div className="border-neutral-400 h-full">
             <ListaPaginada />
