70be9572284ba17b4d6609a20930357b5edd6cb3
Pedro Caetano <pedro.kaetano2003@gmail.com>
2025-09-20 19:14:28 -0300

    Arrumando bug das cifras

--- src/components/saveMusic.tsx
+++ src/components/saveMusic.tsx
@@ -2,7 +2,8 @@ import * as Dialog from '@radix-ui/react-dialog';
 import { useState, useEffect } from 'react';
 import { useMusicContext } from './musicContext';
 import { DragAndDrop, DndItem } from './dragAndDrop';
-import { X, Trash2, SendHorizontal, ListX } from 'lucide-react';
+import { X, Trash2, SendHorizontal, ListX, Music } from 'lucide-react';
+import Cifra from './cifra';
 // import Cifra from './cifra';
 // import { letras } from './letras';
 // import LetraDialog from './letraDialog';
@@ -79,6 +80,29 @@ export function SaveMusic() {
                         {/* <div>
                           <LetraDialog key={Number(music.id)} music={musicItem} />
                         </div> */}
+                        <Dialog.Root>
+                          <Dialog.Trigger asChild>
+                            <Music className='size-5 hover:scale-125 hover:cursor-pointer hover:animate-pulse duration-300' />
+                          </Dialog.Trigger>
+
+                          <Dialog.Portal>
+                            <Dialog.Overlay />
+                            <Dialog.Content
+                              className="fixed inset-0 tablet:inset-auto tablet:left-1/2 tablet:top-1/2
+                              tablet:-translate-x-1/2 tablet:-translate-y-1/2 bg-[#181f2c] tablet:rounded-md
+                              flex outline-none w-[90%] h-[90%] celular:w-[100%] celular:h-[100%] p-6 justify-center overflow-y-auto scrollbar scrollbar-thumb-gray-800 flex-1"
+                            >
+                              <Dialog.Close className="absolute right-0 top-0 p-1.5 text-slate-400 hover:text-slate-100">
+                                <X className="m-3" />
+                              </Dialog.Close>
+                              <div className=' flex w-full justify-center text-left'>
+                                {/* <div className='justify-left'> */}
+                                  <Cifra musicaId={Number(music.id)}/>
+                                {/* </div> */}
+                              </div>
+                            </Dialog.Content>
+                          </Dialog.Portal>
+                        </Dialog.Root>
                         {/* <div>
                           { music.cantor ? (
                             <a href={music.cantor} target="_blank" rel="noopener noreferrer">
