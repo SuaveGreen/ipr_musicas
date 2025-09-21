3f01011f920ba7dd97697a097edbc5a4fdf9538c
Pedro Caetano <86443721+SuaveGreen@users.noreply.github.com>
2025-09-21 11:40:17 -0300

    Atualizar o saveMusic.tsx

--- src/components/saveMusic.tsx
+++ src/components/saveMusic.tsx
@@ -97,7 +97,7 @@ export function SaveMusic() {
                               <Dialog.Portal>
                                 <Dialog.Overlay />
                                 <Dialog.Content className="fixed inset-0 tablet:inset-auto tablet:left-1/2 tablet:top-1/2
-                                    tablet:-translate-x-1/2 tablet:-translate-y-1/2 bg-[#000000] tablet:rounded-md
+                                    tablet:-translate-x-1/2 tablet:-translate-y-1/2 bg-[#FFFFFF] tablet:rounded-md
                                     flex outline-none w-[90%] h-[90%] celular:w-[100%] celular:h-[100%] p-6 justify-center overflow-y-auto scrollbar scrollbar-thumb-gray-800 flex-1">
 
                                   <Dialog.Close className="absolute right-0 top-0 p-1.5 text-slate-400 hover:text-slate-100">
